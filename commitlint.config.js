eb11f5255af17ed70017f407b23d35d0ac53aabd
Tim Beyer <tim.beyer@gmail.com>
2025-07-30 10:01:56 +0200

    chore: disable commitlint subject case check
    - Disable subject-case rule to allow constant names like ROOT_COLUMN_OFFSET
    - Keep other conventional commit checks intact
    - Allow flexible subject formatting for technical terms

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,7 @@ export default {
   extends: ['@commitlint/config-conventional'],
   ignores: [commit => commit === 'Initial plan'],
   rules: {
-    'body-max-line-length': [0, 'always'] // Disable body line length check
+    'body-max-line-length': [0, 'always'], // Disable body line length check
+    'subject-case': [0] // Disable subject case check to allow constant names like ROOT_COLUMN_OFFSET
   }
 }
